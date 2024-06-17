fc0b75b2359eb3ca7d9ee26f582310817dd2f019
Ilya30432 <IlyaS30432@gmail.com>
2024-06-17 22:13:57 +0200

    hw-28

--- react-project/src/components/Table/Table.js
+++ react-project/src/components/Table/Table.js
@@ -3,37 +3,34 @@ import { BiSolidPencil } from "react-icons/bi";
 import { FaTrashCan } from "react-icons/fa6";
 import vector from "./vector.svg";
 
-const titleInfo = ["Id","Category","Name","Quantity","Price(₴)"]
+const titleInfo = ["Id", "Category", "Name", "Quantity", "Price(₴)"];
 
-const productInfo = [
-  { id: 0, category: "Pc", name: "Lenovo", quantity: 5, price: 25000 },
-  { id: 1, category: "Tablet", name: "Ipad", quantity: 5, price: 29000 },
-  { id: 2, category: "Notebook", name: "Asus", quantity: 5, price: 34000 },
-];
-
-function Table({}) {
+function Table({products}) {
   return (
     <table className="table-product">
       <thead>
-        <tr className = "title-row">
-          {titleInfo.map((info,index) => (
-            <td key={index} className="title-info">{info}
-            <img src={vector} alt="vector" className="vector"/>
+        <tr className="title-row">
+          {titleInfo.map((info, index) => (
+            <td key={index} className="title-info">
+              {info}
+              <img src={vector} alt="vector" className="vector" />
             </td>
           ))}
           <td className="title-info"></td>
         </tr>
       </thead>
       <tbody>
-        {productInfo.map((product) => (
+        {products.map((product) => (
           <tr key={product.id} className={`row-${product.id}`}>
-            {Object.values(product).map((info,index) => (
-              <td key={index} className={`info-${product.id}`}>{info}</td>
+            {Object.values(product).map((info, index) => (
+              <td key={index} className={`info-${product.id}`}>
+                {info}
+              </td>
             ))}
             <td>
               <p className="icon">
                 <BiSolidPencil />
-                <FaTrashCan/>
+                <FaTrashCan />
               </p>
             </td>
           </tr>
--- react-project/src/pages/Products/Products.js
+++ react-project/src/pages/Products/Products.js
@@ -4,8 +4,27 @@ import './Products.css';
 import logo from "./rozetka1.svg";
 import { MdOutlinePeopleAlt } from "react-icons/md";
 import { GoPlus } from "react-icons/go";
+import React, { useState, useEffect } from "react";
+import { API_URL } from '../../constants';
 
 function Products() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const infoProduct = async () => {
+      try {
+        const response = await fetch(
+          `${API_URL}/Product`
+        );
+        const data = await response.json();
+        setProducts(data);
+      } catch (error) {
+        console.log("Erorr", error);
+      }
+    };
+    infoProduct();
+  }, []);
+
   return (
     <div className="products">
         <div className ="logos">
@@ -16,7 +35,7 @@ function Products() {
             <Button className= "btn-table" text= "Add product" icon={<GoPlus/>} />
         </div>
         <h2 className = "title">Products</h2>
-        <Table />
+        <Table products={products} />
     </div>
   );
 }
